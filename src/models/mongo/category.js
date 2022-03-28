2a7a6faba6e22efea683e1ffd56208a1b82ef7b0
Egle Budinaviciute <eglebudina@gmail.com>
2022-03-28 18:56:47 +0100

    category

--- src/models/mongo/category.js
+++ src/models/mongo/category.js
@@ -4,11 +4,15 @@ const { Schema } = Mongoose;
 
 const categorySchema = new Schema({
   name: String,
-  img: String,
+  // img: String,
   userid: {
     type: Schema.Types.ObjectId,
     ref: "User",
   },
+  img: {
+    type: Schema.Types.ObjectId,
+    ref: "Image"
+  }
 });
 
 export const Category = Mongoose.model("Category", categorySchema);
