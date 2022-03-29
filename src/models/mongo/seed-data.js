872025ac97dd9a1382a816b85a1faaff99e59310
Egle Budinaviciute <eglebudina@gmail.com>
2022-03-29 21:51:35 +0100

    seed images

--- src/models/mongo/seed-data.js
+++ src/models/mongo/seed-data.js
@@ -28,7 +28,19 @@ export const seedData = {
     _model: "Category",
     kerry: {
       name: "Kerry",
-      userid: "->users.homer"
+      userid: "->users.homer",
+      img: "https://res.cloudinary.com/dxrvgvnzb/image/upload/v1648490779/hng454i1y3cszcltslbx.jpg"
+      
+    },
+    wicklow: {
+      name: "Wicklow",
+      userid: "->users.homer",
+      img: "https://res.cloudinary.com/dxrvgvnzb/image/upload/v1648377258/wicklow-category_x31nw1.jpg"
+    },
+    donegal: {
+      name: "Donegal",
+      userid: "->users.homer",
+      img: "https://res.cloudinary.com/dxrvgvnzb/image/upload/v1648377369/donegal-category_tikrik.jpg"
     }
   },
   places: {
@@ -40,5 +52,26 @@ export const seedData = {
       longitude: 9.7432,
       categoryid: "->categorys.kerry"
     },
+    place_2 : {
+      name: "The_Glen_of_Aherlow",
+      description: "Trek to Lough Curra, Medium",
+      latitude: 52.4169,
+      longitude: 8.1444,
+      categoryid: "->categorys.kerry"
+    },
+    place_3 : {
+      name: "Errigal_Mountain",
+      description: "Trek to Donegal skyline, Medium",
+      latitude: 55.0343,
+      longitude: 8.1130,
+      categoryid: "->categorys.donegal"
+    },
+    place_4 : {
+      name: "Djouce",
+      description: "Hike in Wicklow Mountain National Park, Medium",
+      latitude: 53.1305,
+      longitude: 6.2393,
+      categoryid: "->categorys.wicklow"
+    }
   }
 };
